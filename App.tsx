57adb975ee6ddaf6a48981772b6bd6e5d8eb9157
ParthLashkari <parthlashkari11@gmail.com>
2023-06-27 00:25:47 +0530

    First commit

---
+++ App.tsx
@@ -0,0 +1,110 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  Image,
+  ImageBackground,
+  PixelRatio,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import Rectangle1 from "./src/assets/Rectangle1.png"
+import roombudy from "./src/assets/roombudy.png"
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+  listenOrientationChange as lor,
+  removeOrientationListener as rol
+} from 'react-native-responsive-screen';
+
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+function Section({children, title}: SectionProps): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+}
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <ImageBackground source={Rectangle1} resizeMode='cover' imageStyle={{borderBottomLeftRadius:29, borderBottomRightRadius:29}} style={{height: hp(60), justifyContent:'center', alignItems:'center'}}>
+          <Text style={{color:'white', fontSize: 30/ PixelRatio.getFontScale(), fontFamily:'Jomhuria', fontWeight:800, fontStyle:'bold'}}>Welcome To</Text>
+            {/* <Image source={roombudy}/> */}
+            <Text style={{color:'white', fontSize: 48/ PixelRatio.getFontScale(), fontFamily:'Luckiest Guy', fontWeight:800, fontStyle:'bold'}}>RoomBudy</Text>
+          </ImageBackground>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
